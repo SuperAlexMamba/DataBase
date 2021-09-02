03233d1ffb964f43ef2fd137ceb8859b13a58abb
SuperAlexMamba <slava.orlob@gmail.com>
2021-09-02 13:52:08 +0300

    New Class CustomTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Слава Орлов on 02.09.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameOfPlace: UILabel!
+    @IBOutlet weak var locationOfPlace: UILabel!
+    @IBOutlet weak var typeOfPlace: UILabel!
+    
+}
